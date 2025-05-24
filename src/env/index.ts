e65a232dca2940f7cd258d3ca769ce75929ca260
matheuspimenta <1matheuspimentel@gmail.com>
2025-05-24 15:44:41 -0300

    marcha

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,22 +1,25 @@
-// src/env.ts
-import { config as loadEnv } from './env'
+import { config } from 'dotenv'
 import { z } from 'zod'
 
-// Carrega as variáveis do arquivo .env
-loadEnv()
+if (process.env.NODE_ENV === 'test') {
+  config({ path: '.env.test' })
+} else {
+  config()
+}
 
 const envSchema = z.object({
   NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
-  DATABASE_CLIENT: z.enum(['sqlite', 'pg']),
+  DATABASE_CLIENT: z.enum(['sqlite', 'pg']).default('sqlite'),
   DATABASE_URL: z.string(),
   PORT: z.coerce.number().default(3333),
 })
 
 const _env = envSchema.safeParse(process.env)
 
-if (!_env.success) {
-  console.error('❌ Erro nas variáveis de ambiente:', _env.error.format())
-  process.exit(1)
+if (_env.success === false) {
+  console.error('⚠️ Invalid environment variables', _env.error.format())
+
+  throw new Error('Invalid environment variables.')
 }
 
 export const env = _env.data
